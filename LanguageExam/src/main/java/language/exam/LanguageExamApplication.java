42114bc3cd03bc6121747763f7f0fee6fd9be664
ja784a <kouheikun0124@gmail.com>
2025-05-23 15:04:10 +0900

    commit

---
+++ LanguageExam/src/main/java/language/exam/LanguageExamApplication.java
@@ -0,0 +1,13 @@
+package language.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageExamApplication.class, args);
+	}
+
+}
---
+++ LanguageExam/src/test/java/language/exam/LanguageExamApplicationTests.java
@@ -0,0 +1,13 @@
+package language.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
