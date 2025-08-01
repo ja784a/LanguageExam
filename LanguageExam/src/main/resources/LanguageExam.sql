8f8118e3d88863a29047e27c8b10656e4808842e
ja784a <kouheikun0124@gmail.com>
2025-08-01 14:14:00 +0900

    mavenの更新など

---
+++ LanguageExam/src/main/resources/LanguageExam.sql
@@ -0,0 +1,24 @@
+create table subjects (
+id bigint not null primary key, 
+subject varchar(50) not null )
+
+create table grades (
+id bigint not null primary key, 
+grade varchar(59) not null ) 
+
+create table fees (
+id bigint not null primary key, 
+subject_id bigint not null, 
+grade_id bigint not null, 
+fee bigint not null ) 
+
+create table exam_infos (
+id bigint not null primary key, 
+subject_id bigint not null, 
+grade_id bigint not null, 
+exam_date date not null, 
+old_date date, 
+place varchar(50) not null, 
+comments varchar(100), 
+cancel bigint not null default 0 ) 
+
---
+++ LanguageExam/src/main/resources/insert.sql
@@ -0,0 +1,17 @@
+insert into subjects (subject) values 
+('英語試験'), 
+('中国語試験'), 
+('韓国語試験') 
+
+insert into grdes (grade) 
+('初級'), 
+('中級'), 
+('上級') 
+
+insert into fees (subject_id, grade_id, fee) values 
+(1, 1, 5000), 
+(1, 2, 6000), 
+(1, 3, 
+
+insert into exam_infos (suject_id, grade_id, exam_date, place) values 
+(1, 1, 2025-08-01, '大阪') 
