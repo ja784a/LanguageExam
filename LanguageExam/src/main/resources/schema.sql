92caf1542e1f30682cad2f507b0dc22ab99257a5
ja784a <kouheikun0124@gmail.com>
2025-11-04 14:50:04 +0900

    Merge remote-tracking branch 'origin/main' into main

--- LanguageExam/src/main/resources/schema.sql
+++ LanguageExam/src/main/resources/schema.sql
@@ -1,4 +1,7 @@
--- accountsテーブル作成用
+create database LanguageExam;
+
+use LanguageExam;
+
 create table accounts (
 id bigint not null auto_increment primary key, 
 name varchar(50) not null, 
@@ -10,29 +13,24 @@ town varchar(50) not null,
 building varchar(50) , 
 role tinyint not null default 0 );
 
--- subjectsテーブル作成用
 create table subjects (
 id bigint not null auto_increment primary key, 
 subject varchar(50) not null );
 
--- gradesテーブル作成用
 create table grades (
 id bigint not null auto_increment primary key, 
 grade varchar(50) not null ); 
 
--- placesテーブル作成用
 create table places (
 id bigint not null auto_increment primary key,
 place varchar(50) not null ); 
 
--- feesテーブル作成用
 create table fees (
 id bigint not null auto_increment primary key, 
 subject_id bigint not null, 
 grade_id bigint not null, 
 fee bigint ); 
 
--- exam_infosテーブル作成用
 create table exam_infos ( 
 id bigint not null auto_increment primary key, 
 subject_id bigint not null, 
@@ -43,13 +41,11 @@ place_id bigint not null,
 comments varchar(100), 
 cancel tinyint not null default 0 ); 
 
--- bookingsテーブル作成用
 create table bookings ( 
 id bigint not null auto_increment primary key, 
 account_id bigint not null, 
 exam_id bigint not null ); 
 
--- infosテーブル作成用
 create table infos ( 
 id bigint not null auto_increment primary key, 
 post_date date not null, 
